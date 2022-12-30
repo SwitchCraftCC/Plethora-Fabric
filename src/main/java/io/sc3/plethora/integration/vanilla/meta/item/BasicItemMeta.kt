8790760e3f5c4804e5bba3f11f3d04c91a0383ca
Drew Edwards <github@drew.contact>
2022-12-30 23:04:20 +0000

    feat: tag meta providers

--- src/main/java/io/sc3/plethora/integration/vanilla/meta/item/BasicItemMeta.kt
+++ src/main/java/io/sc3/plethora/integration/vanilla/meta/item/BasicItemMeta.kt
@@ -1,71 +1,79 @@
-package io.sc3.plethora.integration.vanilla.meta.item;
+package io.sc3.plethora.integration.vanilla.meta.item
 
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.registry.Registries;
-import net.minecraft.text.Text;
-import io.sc3.plethora.api.meta.BasicMetaProvider;
+import dan200.computercraft.shared.util.NBTUtil
+import io.sc3.plethora.Plethora
+import io.sc3.plethora.api.meta.BasicMetaProvider
+import net.minecraft.block.Block
+import net.minecraft.block.Blocks
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtElement
+import net.minecraft.registry.Registries
+import java.util.stream.Collectors
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+class BasicItemMeta : BasicMetaProvider<ItemStack>() {
+  override fun getMeta(stack: ItemStack): Map<String, *> {
+    if (stack.isEmpty) return emptyMap<String, Any>()
 
-import static dan200.computercraft.shared.util.NBTUtil.getNBTHash;
+    val data = HashMap<String, Any?>()
+    fillBasicMeta(data, stack)
 
-public final class BasicItemMeta extends BasicMetaProvider<ItemStack> {
-    @Nonnull
-    @Override
-    public Map<String, ?> getMeta(@Nonnull ItemStack stack) {
-        if (stack.isEmpty()) return Collections.emptyMap();
+    val displayText = stack.name
+    val display = displayText?.string
 
-        HashMap<String, Object> data = new HashMap<>();
-        fillBasicMeta(data, stack);
+    data["displayName"] = if (display.isNullOrEmpty()) stack.translationKey else display
+    data["rawName"] = stack.translationKey
 
-        Text displayText = stack.getName();
-        String display = displayText != null ? displayText.getString() : null;
-        data.put("displayName", display == null || display.isEmpty() ? stack.getTranslationKey() : display);
-        data.put("rawName", stack.getTranslationKey());
+    data["maxCount"] = stack.maxCount
+    data["maxDamage"] = stack.maxDamage
 
-        data.put("maxCount", stack.getMaxCount());
-        data.put("maxDamage", stack.getMaxDamage());
-
-        if (stack.getItem().isItemBarVisible(stack)) {
-            data.put("durability", stack.getItem().getItemBarStep(stack));
-        }
-
-        NbtCompound nbt = stack.getNbt();
-        if (nbt != null && nbt.contains("display", NbtElement.COMPOUND_TYPE)) {
-            NbtCompound displayNbt = nbt.getCompound("display");
-            if (displayNbt.contains("Lore", NbtElement.LIST_TYPE)) {
-                NbtList loreNbt = displayNbt.getList("Lore", NbtElement.STRING_TYPE);
-                data.put("lore", loreNbt.stream().map(NbtElement::asString).collect(Collectors.toList()));
-            }
-        }
-
-        return data;
+    if (stack.item.isItemBarVisible(stack)) {
+      data["durability"] = stack.item.getItemBarStep(stack)
     }
 
-    @Nonnull
-    public static HashMap<String, Object> getBasicMeta(@Nonnull ItemStack stack) {
-        HashMap<String, Object> data = new HashMap<>();
-        fillBasicMeta(data, stack);
-        return data;
+    val nbt = stack.nbt
+    if (nbt != null && nbt.contains("display", NbtElement.COMPOUND_TYPE.toInt())) {
+      val displayNbt = nbt.getCompound("display")
+      if (displayNbt.contains("Lore", NbtElement.LIST_TYPE.toInt())) {
+        val loreNbt = displayNbt.getList("Lore", NbtElement.STRING_TYPE.toInt())
+        data["lore"] = loreNbt.stream().map(NbtElement::asString).collect(Collectors.toList())
+      }
     }
 
-    public static void fillBasicMeta(HashMap<String, Object> data, ItemStack stack) {
-        data.put("name", Registries.ITEM.getId(stack.getItem()).toString());
-        data.put("damage", stack.getDamage());
-        data.put("count", stack.getCount());
-        data.put("nbt", getNbtHash(stack));
+    val tags = mutableSetOf<String>()
+    try {
+      // Add all the item tags
+      stack.streamTags().forEach { t -> tags.add(t.id().toString()) }
+
+      // If this is a block item, add the block tags too. Default implementation of getBlockFromItem is to check if
+      // the item is an instance of BlockItem, but a few blocks are known to have their own item classes and will
+      // override this.
+      val block = Block.getBlockFromItem(stack.item)
+      if (block !== Blocks.AIR) {
+        Registries.BLOCK.getEntry(block).streamTags().forEach { t -> tags.add(t.id().toString()) }
+      }
+    } catch (e: Exception) {
+      Plethora.log.error("Failed to get tags for item {}", stack, e)
+    }
+    data["tags"] = tags
+
+    return data
+  }
+
+  companion object {
+    fun getBasicMeta(stack: ItemStack): HashMap<String, Any?> {
+      val data = HashMap<String, Any?>()
+      fillBasicMeta(data, stack)
+      return data
     }
 
-    @Nullable
-    public static String getNbtHash(@Nonnull ItemStack stack) {
-        return stack.hasNbt() ? getNBTHash(stack.getNbt()) : null;
+    fun fillBasicMeta(data: HashMap<String, Any?>, stack: ItemStack) {
+      data["name"] = Registries.ITEM.getId(stack.item).toString()
+      data["damage"] = stack.damage
+      data["count"] = stack.count
+      data["nbt"] = getNbtHash(stack)
     }
+
+    private fun getNbtHash(stack: ItemStack): String? =
+      if (stack.hasNbt()) NBTUtil.getNBTHash(stack.nbt) else null
+  }
 }
