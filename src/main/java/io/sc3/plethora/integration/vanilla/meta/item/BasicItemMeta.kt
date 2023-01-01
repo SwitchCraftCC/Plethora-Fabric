aba5b6346207b349872d520546e73d04d19a32a8
Drew Edwards <github@drew.contact>
2023-01-01 11:48:58 +0000

    fix: remove unnecessary meta providers

--- src/main/java/io/sc3/plethora/integration/vanilla/meta/item/BasicItemMeta.kt
+++ src/main/java/io/sc3/plethora/integration/vanilla/meta/item/BasicItemMeta.kt
@@ -1,14 +1,9 @@
 package io.sc3.plethora.integration.vanilla.meta.item
 
 import dan200.computercraft.shared.util.NBTUtil
-import io.sc3.plethora.Plethora
 import io.sc3.plethora.api.meta.BasicMetaProvider
-import net.minecraft.block.Block
-import net.minecraft.block.Blocks
 import net.minecraft.item.ItemStack
-import net.minecraft.nbt.NbtElement
 import net.minecraft.registry.Registries
-import java.util.stream.Collectors
 
 class BasicItemMeta : BasicMetaProvider<ItemStack>() {
   override fun getMeta(stack: ItemStack): Map<String, *> {
@@ -29,32 +24,6 @@ class BasicItemMeta : BasicMetaProvider<ItemStack>() {
     if (stack.item.isItemBarVisible(stack)) {
       data["durability"] = stack.item.getItemBarStep(stack)
     }
-
-    val nbt = stack.nbt
-    if (nbt != null && nbt.contains("display", NbtElement.COMPOUND_TYPE.toInt())) {
-      val displayNbt = nbt.getCompound("display")
-      if (displayNbt.contains("Lore", NbtElement.LIST_TYPE.toInt())) {
-        val loreNbt = displayNbt.getList("Lore", NbtElement.STRING_TYPE.toInt())
-        data["lore"] = loreNbt.stream().map(NbtElement::asString).collect(Collectors.toList())
-      }
-    }
-
-    val tags = mutableSetOf<String>()
-    try {
-      // Add all the item tags
-      stack.streamTags().forEach { t -> tags.add(t.id().toString()) }
-
-      // If this is a block item, add the block tags too. Default implementation of getBlockFromItem is to check if
-      // the item is an instance of BlockItem, but a few blocks are known to have their own item classes and will
-      // override this.
-      val block = Block.getBlockFromItem(stack.item)
-      if (block !== Blocks.AIR) {
-        Registries.BLOCK.getEntry(block).streamTags().forEach { t -> tags.add(t.id().toString()) }
-      }
-    } catch (e: Exception) {
-      Plethora.log.error("Failed to get tags for item {}", stack, e)
-    }
-    data["tags"] = tags
 
     return data
   }
