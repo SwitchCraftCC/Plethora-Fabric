eb3ec28b06b3242198fbb1bf548bb4d6d290951f
Todd <hugeblank@gmail.com>
2023-05-10 11:20:57 -0700

    fix: entity motion inconsistencies (#55)
    Fixes SwitchCraftCC/Plethora-Fabric#52.   Adds a pair of mixins that calculate velocity from the current position and the position of the entity in the tick prior.
    
    ---------
    
    Co-authored-by: Drew Edwards <github@drew.contact>

---
+++ src/main/java/io/sc3/plethora/mixin/EntityMixin.java
@@ -0,0 +1,41 @@
+package io.sc3.plethora.mixin;
+
+import io.sc3.plethora.util.VelocityDeterminable;
+import net.minecraft.entity.Entity;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(Entity.class)
+public class EntityMixin implements VelocityDeterminable {
+  @Unique
+  private Vec3d prevPos = Vec3d.ZERO;
+  @Unique
+  private Vec3d motion = Vec3d.ZERO;
+  @Unique
+  private RegistryKey<World> prevWorld;
+
+  @Override
+  public void storePrevPos() {
+    var entity = (Entity) (Object) this;
+    var pos = entity.getPos();
+    var world = entity.getWorld().getRegistryKey();
+
+    // Avoid velocity spikes when changing dimensions
+    if (prevWorld != world) {
+      prevWorld = world;
+      motion = Vec3d.ZERO;
+    } else {
+      motion = pos.subtract(prevPos);
+    }
+
+    prevPos = pos;
+  }
+
+  @Override
+  public Vec3d getMotion() {
+        return motion;
+    }
+}
---
+++ src/main/java/io/sc3/plethora/mixin/ServerWorldMixin.java
@@ -0,0 +1,22 @@
+package io.sc3.plethora.mixin;
+
+import io.sc3.plethora.util.VelocityDeterminable;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerWorld.class)
+public class ServerWorldMixin {
+    @Inject(at = @At("TAIL"), method = "tickEntity(Lnet/minecraft/entity/Entity;)V")
+    private void setPrevPos(Entity entity, CallbackInfo ci) {
+        ((VelocityDeterminable)entity).storePrevPos();
+    }
+
+  @Inject(at = @At("TAIL"), method = "tickPassenger(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/Entity;)V")
+  private void setPrevPosRiding(Entity vehicle, Entity passenger, CallbackInfo ci) {
+    ((VelocityDeterminable)passenger).storePrevPos();
+  }
+}
