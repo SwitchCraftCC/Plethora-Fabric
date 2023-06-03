abe5182c143d8550aa187bd5f2c8278c48af82d6
Drew Edwards <github@drew.contact>
2023-06-03 21:55:43 +0100

    feat: more laser-immune entities

--- src/main/java/io/sc3/plethora/gameplay/data/DamageTypeTagProvider.kt
+++ src/main/java/io/sc3/plethora/gameplay/data/DamageTypeTagProvider.kt
@@ -1,19 +1,21 @@
 package io.sc3.plethora.gameplay.data
 
-import io.sc3.plethora.gameplay.PlethoraEntityTags
+import io.sc3.plethora.gameplay.registry.Registration
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
-import net.minecraft.entity.EntityType
+import net.minecraft.entity.damage.DamageType
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.RegistryWrapper
+import net.minecraft.registry.tag.DamageTypeTags
 import java.util.concurrent.CompletableFuture
 
 class DamageTypeTagProvider(
   out: FabricDataOutput,
   future: CompletableFuture<RegistryWrapper.WrapperLookup>
-) : FabricTagProvider<EntityType<*>>(out, RegistryKeys.ENTITY_TYPE, future) {
+) : FabricTagProvider<DamageType>(out, RegistryKeys.DAMAGE_TYPE, future) {
   override fun configure(arg: RegistryWrapper.WrapperLookup) {
-    getOrCreateTagBuilder(PlethoraEntityTags.LASERS_PROVIDE_ENERGY).add(EntityType.BLAZE)
+    getOrCreateTagBuilder(DamageTypeTags.IS_PROJECTILE).add(Registration.ModDamageSources.LASER)
+    getOrCreateTagBuilder(DamageTypeTags.IS_FIRE).add(Registration.ModDamageSources.LASER)
   }
 }
 
--- src/main/java/io/sc3/plethora/gameplay/data/EntityTypeTagProvider.kt
+++ src/main/java/io/sc3/plethora/gameplay/data/EntityTypeTagProvider.kt
@@ -1,21 +1,20 @@
 package io.sc3.plethora.gameplay.data
 
-import io.sc3.plethora.gameplay.registry.Registration.ModDamageSources
+import io.sc3.plethora.gameplay.PlethoraEntityTags
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
-import net.minecraft.entity.damage.DamageType
+import net.minecraft.entity.EntityType
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.RegistryWrapper
-import net.minecraft.registry.tag.DamageTypeTags
 import java.util.concurrent.CompletableFuture
 
 class EntityTypeTagProvider(
   out: FabricDataOutput,
   future: CompletableFuture<RegistryWrapper.WrapperLookup>
-) : FabricTagProvider<DamageType>(out, RegistryKeys.DAMAGE_TYPE, future) {
+) : FabricTagProvider<EntityType<*>>(out, RegistryKeys.ENTITY_TYPE, future) {
   override fun configure(arg: RegistryWrapper.WrapperLookup) {
-    getOrCreateTagBuilder(DamageTypeTags.IS_PROJECTILE).add(ModDamageSources.LASER)
-    getOrCreateTagBuilder(DamageTypeTags.IS_FIRE).add(ModDamageSources.LASER)
+    getOrCreateTagBuilder(PlethoraEntityTags.LASERS_PROVIDE_ENERGY)
+      .add(EntityType.BLAZE, EntityType.MAGMA_CUBE, EntityType.ZOMBIFIED_PIGLIN)
   }
 }
 
