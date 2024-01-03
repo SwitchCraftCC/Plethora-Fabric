147df8258df5a5e88ab52e5871657a3b51c5310a
Jonathan Coates <git@squiddev.cc>
2024-01-03 21:22:00 +0000

    Add back computer family meta details

--- src/main/java/io/sc3/plethora/integration/computercraft/meta/item/ComputerItemMeta.kt
+++ src/main/java/io/sc3/plethora/integration/computercraft/meta/item/ComputerItemMeta.kt
@@ -1,7 +1,9 @@
 package io.sc3.plethora.integration.computercraft.meta.item
 
+import dan200.computercraft.shared.ModRegistry
 import dan200.computercraft.shared.computer.items.IComputerItem
 import io.sc3.plethora.api.meta.ItemStackMetaProvider
+import io.sc3.plethora.gameplay.registry.Registration.ModItems
 import net.minecraft.item.ItemStack
 
 class ComputerItemMeta : ItemStackMetaProvider<IComputerItem>(IComputerItem::class.java, "computer") {
@@ -13,6 +15,14 @@ class ComputerItemMeta : ItemStackMetaProvider<IComputerItem>(IComputerItem::cla
 
     val label = item.getLabel(stack)
     if (!label.isNullOrEmpty()) data["label"] = label
+
+    // "family" used to be available on IComputerItem. Instead, we just look it up based on the current item.
+    data["family"] = when (item) {
+      ModRegistry.Items.COMPUTER_NORMAL.get(), ModRegistry.Items.TURTLE_NORMAL.get(), ModRegistry.Items.POCKET_COMPUTER_NORMAL.get() -> "normal"
+      ModRegistry.Items.COMPUTER_ADVANCED.get(), ModRegistry.Items.TURTLE_ADVANCED.get(), ModRegistry.Items.POCKET_COMPUTER_ADVANCED.get(), ModItems.NEURAL_INTERFACE -> "advanced"
+      ModRegistry.Items.COMPUTER_COMMAND.get() -> "command"
+      else -> "unknown"
+    }
 
     return data
   }
